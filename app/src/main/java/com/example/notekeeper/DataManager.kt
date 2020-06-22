ffc7e4ed950b6657d985ad64944be5ef3ea33cf7
missHS <helene.schonbeck@gmail.com>
2020-06-22 16:08:55 +0200

    .

---
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -0,0 +1,23 @@
+package com.example.notekeeper
+
+import android.provider.ContactsContract
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init{
+        initializeCourses()
+    }
+
+    private fun initializeCourses(){
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course )
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title="Java Fundamentals: The Java Language", courseId = "java_fundamentals")
+        courses.set(course.courseId, course)
+    }
+}
